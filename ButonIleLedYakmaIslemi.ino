0953104b9cd4d2998f45dafb157613060cda5ee7
esatgozcu <35576161+esatgozcu@users.noreply.github.com>
2018-06-07 05:48:29 +0300

    Add files via upload

---
+++ ButonIleLedYakmaIslemi.ino
@@ -0,0 +1,20 @@
+int butonDurumu;
+
+void setup() {
+
+  pinMode(8,OUTPUT);
+  pinMode(9,INPUT);
+}
+
+void loop() {
+
+  butonDurumu = digitalRead(9);
+  if(butonDurumu == HIGH)
+  {
+    digitalWrite(8,HIGH);
+  }
+  else 
+  {
+    digitalWrite(8,LOW);
+  }
+}
